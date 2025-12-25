0bd0249a51b9031f532bb379271b6702f7e685e1
duan <762842077@qq.com>
2025-12-25 20:50:07 +0800

    aaa

---
+++ scripts/utils.sh
@@ -0,0 +1,9 @@
+
+
+# 根据sae生成lut
+python convert_sae_to_lut.py /root/models/Qwen3-0.6B /root/sparsify/checkpoints/ \
+      --output_dir ./lut_output \
+      --proj_types qkv gate_up oproj \
+      --layers 0-27 \
+      --threshold_dir /root/sparsify/thresholds/Qwen3-0.6B \
+      --dtype bfloat16
